69fe512e1c08765d4be7d2d46f9753140c318fa4
Aleksandr Karpov <karpov@sbtcom.ru>
2016-11-14 12:59:56 +0300

    Add empty class

---
+++ app/src/main/java/io/github/keyfour13/androidrandomlayoutexample/RandomLayout.java
@@ -0,0 +1,16 @@
+package io.github.keyfour13.androidrandomlayoutexample;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class RandomLayout extends ViewGroup {
+
+    public RandomLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
